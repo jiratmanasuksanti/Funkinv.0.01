d68c74b4354ea228288050e88f582904f3df2660
jiratmanasuksanti <86285957+jiratmanasuksanti@users.noreply.github.com>
2021-06-24 09:17:27 +0700

    Update Funkin.py

--- Funkin.py
+++ Funkin.py
@@ -5,3 +5,9 @@ pygame.init()
 Width = 800
 Height = 600
 screen = pygame.display.set_mode((Width,Height), RESIZEABLE)
+mainloop = True
+################Main Game Loop#################
+while mainloop:
+  for event in pygame.event.get():
+    if event.type == pygame.QUIT:
+      exit()
