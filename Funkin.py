f7aa720bc58a21bef5f4228f7d7ed638afd7ad1b
jiratmanasuksanti <86285957+jiratmanasuksanti@users.noreply.github.com>
2021-06-24 13:27:24 +0700

    Update Funkin.py

--- Funkin.py
+++ Funkin.py
@@ -1,12 +1,26 @@
 import pygame
 import math
+import time
 #################
 pygame.init()
 Width = 800
 Height = 600
-screen = pygame.display.set_mode((Width,Height), RESIZEABLE)
+screen = pygame.display.set_mode((Width,Height), pygame.FULLSCREEN)
 mainloop = True
+############################Game Object Storage##############################
+bopeebo_inst = pygame.mixer.music.load("assets/music/Bopeebo_Inst.ogg")
+bopeebo_voices = pygame.mixer.music.load("assets/music/Bopeebo_Voices.ogg")
+exitbutton_image = pygame.image.load("assets/images/exit_button.png")
+pressed_exitbutton_image = pygame.image.load("assets/images/pressed_exit_button.png")
+menu_music = pygame.mixer.music.load("assets/music/freakyMenu.ogg")
 ################Main Game Loop#################
+def BacktoMenu():
+  if menu_music_play == False:
+    menu_music_play = True
+    pygame.mixer.music.stop()
+    menu_music.play()
+menu_music_play = False
+BacktoMenu()
 while mainloop:
   for event in pygame.event.get():
     if event.type == pygame.QUIT:
